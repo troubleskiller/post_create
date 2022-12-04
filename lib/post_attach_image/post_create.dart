718a8ca18e60d9cae33c0a302c54961afec26c08
troubleskiller <troubleskiller@outlook.com>
2022-12-04 15:57:17 +0800

    完善功能

--- lib/post_attach_image/post_create.dart
+++ lib/post_attach_image/post_create.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:post_create/model/image_model.dart';
 import 'package:post_create/util_widget/gestures.dart';
 
 import '../RTE/RTE_textfield.dart';
 import '../RTE/RTE_textfield_consumer.dart';
 import '../RTE/keyboard_toolbar.dart';
 import '../controller/create_post_provider_controller.dart';
+import 'create_post_attached_files.dart';
 import 'create_post_attached_images.dart';
 
 class PostCreate extends StatefulWidget {
-  const PostCreate({Key? key}) : super(key: key);
+  const PostCreate({
+    Key? key,
+    this.decoration,
+    this.placeholder,
+    this.existingDeltaContent,
+    this.existingImages,
+    this.existingfiles,
+    this.canUploadImage = true,
+    this.canUploadFile = true,
+    this.titleWidget,
+    required this.postAs,
+  }) : super(key: key);
+
+  final BoxDecoration? decoration;
+
+  //AppBar
+  final Widget? titleWidget;
+
+  //占位
+  final String? placeholder;
+
+  //已经有的内容，可用于编辑功能。
+  final String? existingDeltaContent;
+
+  //已经存在的上传图片
+  final List<BubblePostAttachedImageFileModel>? existingImages;
+
+  //已经存在的上传文件
+  final List<BubblePostAttachedImageFileModel>? existingfiles;
+
+  //是否包含上传图片
+  final bool canUploadImage;
+
+  //是否包含上传文件
+  final bool canUploadFile;
+
+  //upload[发布帖子的回调]
+  final Function postAs;
 
   @override
   State<PostCreate> createState() => _PostCreateState();
@@ -17,8 +57,9 @@ class PostCreate extends StatefulWidget {
 class _PostCreateState extends State<PostCreate> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: WithRTE.build(
+    return KeyboardVisibilityBuilder(
+        builder: (keyboardVisibilityContext, isKeyboardVisible) {
+      return WithRTE.build(
         WithCreatePostProvider.build(
             child: DismissKeyboardGesture(
                 bodyContent: Column(
@@ -27,18 +68,39 @@ class _PostCreateState extends State<PostCreate> {
                 flex: 1,
                 child: CustomScrollView(
                   slivers: [
+                    widget.titleWidget ??
+                        AppBar(
+                          title: Text('Create Post'),
+                          actions: [
+                            GestureDetector(
+                              child: Text('Create'),
+                              onTap: () {
+                                widget.postAs();
+                              },
+                            )
+                          ],
+                        ),
                     SliverList(
                         delegate: SliverChildListDelegate([
-                      SizedBox(
-                        height: 100,
-                      ),
                       RTETextFieldConsumer(
-                        placeholder: 'post here',
+                        placeholder: widget.placeholder ?? 'post here',
+                        existingDeltaContent: widget.existingDeltaContent,
                       ),
-                      CreatePostAttachedImageConsumer(
-                        buildContext: context,
-                        isCreatingPost: false,
-                        existingImages: [],
+                      Offstage(
+                        offstage: !widget.canUploadFile,
+                        child: CreatePostAttachedFileConsumer(
+                          buildContext: context,
+                          isCreatingPost: false,
+                          existingFiles: widget.existingfiles ?? [],
+                        ),
+                      ),
+                      Offstage(
+                        offstage: !widget.canUploadImage,
+                        child: CreatePostAttachedImageConsumer(
+                          buildContext: context,
+                          isCreatingPost: false,
+                          existingImages: widget.existingImages ?? [],
+                        ),
                       ),
                     ]))
                   ],
@@ -49,7 +111,7 @@ class _PostCreateState extends State<PostCreate> {
             ),
           ],
         ))),
-      ),
-    );
+      );
+    });
   }
 }
